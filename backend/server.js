df521585e4a0c19366301f988a80dcde43975754
Arthur <arthurd800@wp.pl>
2021-06-04 08:45:18 +0200

    added environmental variables, HomeScreen and ProductScreen fetches data using async functions

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const products = require('./data/products');
 // Home route
@@ -15,4 +17,11 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
