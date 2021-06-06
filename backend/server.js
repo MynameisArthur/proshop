fc65ce6c73171c26b4ca3c3102b65d34403b8513
Arthur <arthurd800@wp.pl>
2021-06-06 11:01:34 +0200

    created data seeder function, connected app with mongo db, created models for user, product and order

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware');
+
 dotenv.config();
+connectDB();
 const app = express();
-const products = require('./data/products');
+const productRoutes = require('./routes/productRoutes');
+
 // Home route
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
-// All products route
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-// Single product route
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 );
