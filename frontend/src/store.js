cd49e4016c8c14c14c51fea191861b050229a5e9
Arthur <arthurd800@wp.pl>
2021-06-08 12:22:38 +0200

    installed redux, setup store, created cart and product actions and reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,22 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-import {productListReducer} from './reducers/productReducer';
+import {
+    productListReducer,
+    productDetailsReducer,
+} from './reducers/productReducer';
+import {cartReducer} from './reducers/cartReducer';
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
+});
 
-const reducer = combineReducers({productList: productListReducer});
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
 
-const initialState = {};
+const initialState = {cart: {cartItems: cartItemsFromStorage}};
 const middleware = [thunk];
 const store = createStore(
     reducer,
