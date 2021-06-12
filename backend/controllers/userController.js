cc3d16993921f4ddcb231e18b8aecfb92f5cd8c3
Arthur <arthurd800@wp.pl>
2021-06-12 11:33:44 +0200

    added updateUserProfile functionality in the backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,5 +72,29 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('Invalid user data');
     }
 });
+// @desc Update user profile
+// @route PUT api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+        const updatedUser = await user.save();
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateToken(updatedUser._id),
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
 
-module.exports = {authUser, getUserProfile, registerUser};
+module.exports = {authUser, getUserProfile, registerUser, updateUserProfile};
