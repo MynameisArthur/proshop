2d8f09d6cc5571efc333b2af05f714321f7abc6b
Arthur <arthurd800@wp.pl>
2021-06-22 11:35:12 +0200

    implemented add image to the product functionality

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
@@ -11,6 +12,7 @@ app.use(express.json());
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes.js');
 
 // Home route
 app.get('/', (req, res) => {
@@ -19,10 +21,12 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
-app.use('/api/config/paypal', (req, res) =>
+app.use('/api/upload', uploadRoutes);
+app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
+// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/uploads', express.static('uploads'));
 app.use(notFound);
 
 app.use(errorHandler);
