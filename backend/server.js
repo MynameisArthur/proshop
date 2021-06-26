37c811fcf3d16e5671bb07e1c59a88c67e8dfc31
Arthur <arthurd800@wp.pl>
2021-06-26 10:05:31 +0200

    modified server.js so application serves static folder when url is /

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
 const uploadRoutes = require('./routes/uploadRoutes.js');
 
-// Home route
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -31,8 +27,20 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 );
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use('/uploads', express.static('uploads'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        );
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
